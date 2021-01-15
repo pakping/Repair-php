d3b5b366637b844e160bb80960ec12f1a49a1241
MainCom <up.localhp@gmail.com>
2021-01-15 14:30:53 +0700

    EEE

--- components/note.php
+++ components/note.php
@@ -3,7 +3,7 @@
     if (isset($_POST['ref'])) {
       $ref = $_POST['ref'];
       $input = $_POST[]
-      $upd = " UPDATE Report SET stat = 'Working' WHERE Case_ID = '$idupd'";
+      $upd = " UPDATE Report SET note = '$input' WHERE Case_ID = '$idupd'";
       $result = mysqli_query($con, $upd);
       if ($result) {
         header('Location: ../app/showdata.php');
