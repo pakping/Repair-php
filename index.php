0196e7bee8e711310b361c045d7675f99f38c4e2
pakping <pakping-up@hotmai.com>
2021-01-19 14:32:07 +0700

    ping

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="./css/style.css" rel="stylesheet">
-  <link href="./bootstrap-5/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-  <script src="./bootstrap-5/js/bootstrap.min.js" crossorigin="anonymous"></script>
-  <?php
+<?php
   session_start();
   if (!isset($_SESSION['type'])){
     $_SESSION['type'] = 'none';
@@ -16,25 +8,22 @@
   }elseif($_SESSION['type'] == 'admin'){
     header("location:app/showdata.php");
   }
-  
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>ระบบแจ้งซ้อม</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="./bootstrap-5/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link href="./css/body.css" rel="stylesheet" crossorigin="anonymous">
+<link rel="icon" href="./img/1200px-UPHosLogo.svg.png" type="image/gif" sizes="16x16">
 </head>
 
 <body>
-
-  <div style="margin-top: 5%;">
-    <!-- <h2>Modal Login Form</h2> -->
-
-    <!-- <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button> -->
-
-    <!-- <div id="id01" class="modal"> -->
-
-    <form class="container" action="auth/checkLogin.php" method="post">
-      <!-- <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div> -->
-
+    <form class="container" action="auth/checkLogin.php" method="post" style="margin-top: 5%;">
       <div class="card col-md-6 offset-md-3" >
         <div class="text-center" style="margin-top: 5%;">
           <img src="https://upload.wikimedia.org/wikipedia/th/thumb/a/a4/UPHosLogo.svg/1024px-UPHosLogo.svg.png"
@@ -42,39 +31,18 @@
           <h2>ลงชื่อเข้าใช้</h2>
           <h6>ระบบ Maintenance Report โรงพยาบาลมหาวิทยาลัยพะเยา</h6>
         </div>
-        <div class="p-2">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="uname" required>
+        <div class="p-5 row">
+          <label for="uname" class="mb-2"><b>Username</b></label>
+          <input class="form-control mb-2" type="text" placeholder="Enter Username" name="uname" required>
 
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
+          <label for="psw" class="mb-2"><b>Password</b></label>
+          <input class="form-control mb-2" type="password" placeholder="Enter Password" name="psw" required>
 
-          <button type="submit" class="btn btn-primary">Login</button>
+          <button type="submit" class="btn btn-primary mt-2">Login</button>
         </div>
-        <!--   <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div> -->
-
-        <!-- <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      
-    </div> -->
     </form>
-    <!-- </div> -->
-  </div>
-  <script>
-    // Get the modal
-    // var modal = document.getElementById('id01');
 
-    // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
-    // }
-  </script>
-
+<script src="./bootstrap-5/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
