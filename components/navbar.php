52eb67e7448b3d39ea061527beb3c7edbe4573c5
pakping <pakping-up@hotmai.com>
2021-01-20 10:44:57 +0700

    ping

--- components/navbar.php
+++ components/navbar.php
@@ -1,32 +1,45 @@
 <?php
 require_once("../DB/connect.php");
 ?>
+
+
 <nav class="navbar navbar-expand-lg navbar-dark"style="background-color: #6f42c1;" >
   <div class="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="..\img\1200px-UPHosLogo.svg.png" alt="" width="30" height="24">
-    </a>
-    <a class="navbar-brand" href="home.php">ระบบแจ้งซ่อมออนไลน์</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <a class="navbar-brand" href="home.php">ระบบแจ้งซ่อมออนไลน์</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <a class="nav-link " href="home.php">แบบฟอร์มแจ้งซ่อม</a>
+          <a class="nav-link active" aria-current="page" href="home.php">แบบฟอร์มแจ้งซ่อม</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="../app/showdatauser.php">แสดงคำร้องข้อมูลแจ้ง</a>
+          <a class="nav-link" href="../app/showdatauser.php">แสดงคำร้องข้อมูลแจ้ง</a>
         </li>
-
       </ul>
-      <form class="d-flex">
-        
-        <a class="navbar-brand" >  <?php
-            echo "<p class='fs-6'>" . $_SESSION['Username'] . "</p>";
-            ?></a>
-        <a  href="../auth/logout.php" class="btn btn-success" role="button">ออกจากระบบ</a>
-      </form>
+
+      <span class="dropdown" class="navbar-text " >
+      <a class="nav-link dropdown-toggle" style="color: #FFFFFF;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" ><img src="../img/profile.png" alt="Avatar" class="avatar">
+      <?php
+            echo "<tr class='fs-4' >". $_SESSION['Username'] . "</tr>";?>
+        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../auth/logout.php" >ออกจากระบบ</a></li>
+          </ul>      
+      </span>
+      &emsp;&emsp;
     </div>
   </div>
 </nav>
+
+<style>.avatar {
+  vertical-align: middle;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+}
+</style>
+
+
+
--- components/navbaradmin.php
+++ components/navbaradmin.php
@@ -1,58 +1,47 @@
 <?php 
 require_once("../DB/connect.php");
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1;">
+
+<nav class="navbar navbar-expand-lg navbar-dark"style="background-color: #6f42c1;" >
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
       <img src="..\img\1200px-UPHosLogo.svg.png" alt="" width="30" height="24">
     </a>
-    <a class="navbar-brand" href="#">ระบบแจ้งซ่อมออนไลน์</a>
+  <a class="navbar-brand" href="#">ระบบแจ้งซ่อมออนไลน์</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-         <!--  <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-        <!-- </li>
-        <li class="nav-item">
-        <a class="nav-link active" href="home.php">แบบฟอร์มแจ้งซ่อม</a>
-        </li> -->
-         
-         <!-- <li class="nav-item">
-        <a class="nav-link" href="../searchdata.php">ค้นหาข้อมูล</a>
+          <a class="nav-link active" aria-current="page" href="../app/showdata.php">แสดงคำร้องข้อมูลแจ้งซ่อม</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="../app/updatedata.php">แก้ไขข้อมูล</a>
+          <a class="nav-link"href="../app/showdatahistory.php">แสดงประวัติการซ่อมแจ้งซ่อม</a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="../app/deletedata.php">ลบข้อมูล</a>
-        </li> -->
-        <li class="nav-item">
-        <a class="nav-link" href="../app/showdata.php">แสดงคำร้องข้อมูลแจ้งซ่อม</a>
-         </li>
-         <li class="nav-item">
-        <a class="nav-link" href="../app/showdatahistory.php">แสดงประวัติการซ่อมแจ้งซ่อม</a>
-         </li>
         <li class="nav-item"> 
         <a class="nav-link" href="../app/showchart.php">สถิติ</a>
         </li>
       </ul>
-      <div class="nav-item">
-        <!-- <span class="navbar-text"> -->
-        <div class="dropdown">
-          <button class="dropstart btn btn-secondary btn-sm dropdown-toggle " type="button" id="dropdownMenuButton"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <?php
-             echo "<p>". $_SESSION['Username'] . "</p>";
-            ?>
-          </button>
-          <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton">
-            <li><a class="dropdown-item" href="../auth/logout.php">ออกจากระบบ</a></li>
-          </ul>
-        </div>
-        <!-- </span> -->
-      </div>
+
+      <span class="dropdown" class="navbar-text " >
+      <a class="nav-link dropdown-toggle" style="color: #FFFFFF;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" ><img src="../img/profile.png" alt="Avatar" class="avatar">
+      <?php
+            echo "<tr class='fs-4' >". $_SESSION['Username'] . "</tr>";?>
+        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../auth/logout.php" >ออกจากระบบ</a></li>
+          </ul>      
+      </span>
+      &emsp;&emsp;
     </div>
   </div>
 </nav>
+
+<style>.avatar {
+  vertical-align: middle;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+}
+</style>
