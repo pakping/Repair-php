02e0f546ddaad9dfe816ba2c41da5240d66b9b85
pakping <pakping-up@hotmai.com>
2021-01-24 12:08:57 +0700

    fix

--- components/meta-title.php
+++ components/meta-title.php
@@ -1,5 +1,4 @@
 <meta charset="utf-8">
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="../bootstrap-5/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 <link href="../css/body.css" rel="stylesheet" crossorigin="anonymous">
