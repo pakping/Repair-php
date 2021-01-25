62d6d47a3d7a9f8b65122e6599f8785c05de05d8
MainCom <up.localhp@gmail.com>
2021-01-25 16:44:10 +0700

    database

--- DB/sqltest1.sql
+++ DB/sqltest1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2021 at 04:46 AM
+-- Generation Time: Jan 25, 2021 at 10:43 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `engineer`
---
-
-CREATE TABLE `engineer` (
-  `Engineer_ID` int(3) NOT NULL,
-  `Engineer_Name` char(20) COLLATE utf32_thai_520_w2 NOT NULL,
-  `Tels.` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_thai_520_w2;
-
---
--- Dumping data for table `engineer`
---
-
-INSERT INTO `engineer` (`Engineer_ID`, `Engineer_Name`, `Tels.`) VALUES
-(3, 'Big', 889943216);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `note`
 --
 
@@ -51,6 +32,13 @@ CREATE TABLE `note` (
   `Username` char(20) COLLATE utf32_thai_520_w2 NOT NULL,
   `Note` varchar(300) COLLATE utf32_thai_520_w2 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_thai_520_w2;
+
+--
+-- Dumping data for table `note`
+--
+
+INSERT INTO `note` (`Case_ID`, `Username`, `Note`) VALUES
+(100, 'Sirichai', 'มยสสย');
 
 -- --------------------------------------------------------
 
@@ -62,7 +50,7 @@ CREATE TABLE `report` (
   `Case_ID` int(8) NOT NULL,
   `Location` char(10) CHARACTER SET utf8 NOT NULL,
   `Problem` char(13) CHARACTER SET utf8 NOT NULL,
-  `Description` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `Description` text CHARACTER SET utf8 NOT NULL,
   `Time` time NOT NULL,
   `Date` date NOT NULL,
   `Editby` char(20) COLLATE utf32_thai_520_w2 NOT NULL,
@@ -76,12 +64,18 @@ CREATE TABLE `report` (
 --
 
 INSERT INTO `report` (`Case_ID`, `Location`, `Problem`, `Description`, `Time`, `Date`, `Editby`, `Stat`, `Username`, `Worker`) VALUES
-(83, 'OPD2', 'Computer', '145195', '10:09:17', '2021-01-21', '', 'รอดำเนินการ', 'phoomin', 'ไม่มี'),
-(84, 'OPD1', 'Computer', '1', '10:16:44', '2021-01-21', '', 'รอดำเนินการ', 'phoomin', 'ไม่มี'),
-(85, 'OPD1', 'Computer', '1', '10:16:48', '2021-01-21', '', 'รอดำเนินการ', 'phoomin', 'ไม่มี'),
-(86, 'OPD1', 'Computer', '1', '10:16:54', '2021-01-21', '', 'รอดำเนินการ', 'phoomin', 'ไม่มี'),
-(87, 'OPD2', 'Printer', '7', '10:17:09', '2021-01-21', '', 'รอดำเนินการ', 'phoomin', 'ไม่มี'),
-(88, 'OPD1', 'Computer', 'aaa', '10:40:06', '2021-01-21', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี');
+(100, 'Office 8', 'Other', 'gfgg', '09:55:58', '2021-01-25', '', 'สำเร็จ', 'Jeerachon', 'Sirichai'),
+(108, 'Emergency', 'Computer', '2', '12:10:04', '2021-01-25', '', 'สำเร็จ', 'Jeerachon', 'Sirichai'),
+(109, 'Emergency', 'Computer', '3', '12:10:09', '2021-01-25', '', 'สำเร็จ', 'Jeerachon', 'Sirichai'),
+(111, 'Emergency', 'Computer', '5', '12:10:20', '2021-01-25', '', 'กำลังดำเนินการ', 'Jeerachon', 'Sirichai'),
+(112, 'IPD1', 'Computer', 'gfhhtr', '12:26:58', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี'),
+(113, 'OPD1', 'Other', 'l;;l', '12:29:27', '2021-01-25', '', 'กำลังดำเนินการ', 'Jeerachon', 'Sirichai'),
+(114, 'IPD1', 'Printer', 'edsgsggdsgsdgdsgsdgs', '12:32:23', '2021-01-25', '', 'สำเร็จ', 'Jeerachon', 'Sirichai'),
+(115, 'IPD1', 'Other', 'hh', '12:39:51', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี'),
+(116, 'OPD1', 'Printer', 'jnn', '12:40:06', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี'),
+(118, 'OPD1', 'Computer', 'fhbfhfcghcvhvcbvcbcvbvcbvcbcvbvcbvcbcvbcvbcv', '12:52:42', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี'),
+(119, 'Office 5', 'Other', 'sadsadsadsadsa', '13:04:21', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี'),
+(120, 'OPD2', 'Printer', 'safdasfdsaf', '16:27:17', '2021-01-25', '', 'รอดำเนินการ', 'Jeerachon', 'ไม่มี');
 
 -- --------------------------------------------------------
 
@@ -109,13 +103,32 @@ INSERT INTO `spare_request` (`Request_ID`, `Case_ID`, `Engineer_ID`, `Req.Descri
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `token_line`
+--
+
+CREATE TABLE `token_line` (
+  `api` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `token_line`
+--
+
+INSERT INTO `token_line` (`api`) VALUES
+('mWLUxFiNjmdgXKZu8oqef6H00OGi6ktec0ftBvhpTs7');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `Username` char(20) COLLATE utf32_thai_520_w2 NOT NULL,
   `Password` char(20) COLLATE utf32_thai_520_w2 NOT NULL,
-  `Tels.` int(10) NOT NULL,
+  `firstname` char(30) COLLATE utf32_thai_520_w2 NOT NULL,
+  `lastname` char(30) COLLATE utf32_thai_520_w2 NOT NULL,
+  `Tel` int(10) NOT NULL,
   `LoginStatus` int(1) NOT NULL,
   `LastUpdate` datetime NOT NULL,
   `Access` char(5) COLLATE utf32_thai_520_w2 NOT NULL
@@ -125,21 +138,15 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`Username`, `Password`, `Tels.`, `LoginStatus`, `LastUpdate`, `Access`) VALUES
-('Jeerachon', '123456', 889943216, 0, '0000-00-00 00:00:00', 'user'),
-('phoomin', '456789', 1333333333, 0, '0000-00-00 00:00:00', 'user'),
-('Sirichai', '654321', 215148148, 0, '0000-00-00 00:00:00', 'admin'),
-('teerat', '987654', 11223344, 0, '0000-00-00 00:00:00', 'admin');
+INSERT INTO `user` (`Username`, `Password`, `firstname`, `lastname`, `Tel`, `LoginStatus`, `LastUpdate`, `Access`) VALUES
+('Jeerachon', '123456', 'จีราชล', 'ธรรมศร', 889943216, 0, '0000-00-00 00:00:00', 'user'),
+('phoomin', '456789', 'ภูมินทร์', 'บุญอนันต์', 1333333333, 0, '0000-00-00 00:00:00', 'user'),
+('Sirichai', '654321', 'ศิริชัย', 'เบ็ญจมาคม', 215148148, 1, '2021-01-25 16:29:18', 'admin'),
+('teerat', '987654', 'ธีรัช', 'กิจเจริญ', 11223344, 0, '0000-00-00 00:00:00', 'admin');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `engineer`
---
-ALTER TABLE `engineer`
-  ADD PRIMARY KEY (`Engineer_ID`);
 
 --
 -- Indexes for table `note`
@@ -175,7 +182,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `report`
 --
 ALTER TABLE `report`
-  MODIFY `Case_ID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `Case_ID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
 
 --
 -- AUTO_INCREMENT for table `spare_request`
