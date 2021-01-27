561cf67016d3cb67748c5b7b1512db0ea7669a89
MainCom <up.localhp@gmail.com>
2021-01-27 11:07:31 +0700

    broder

--- components/footer.php
+++ components/footer.php
@@ -12,7 +12,7 @@
     width: 100%;
   }
 </style>
-
+<!-- data table -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script type="text/javascript" charset="utf8" src="../DataTables/datatables.js"></script>
 
